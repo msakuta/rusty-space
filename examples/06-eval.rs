dd4fa95e1908ee95c260e2c078ed58cc5d99fddd
msakuta <masahiro.sakuta@gmail.com>
2022-09-29 21:59:37 +0900

    Add 06-eval example, a minimal AST interpreter

--- examples/06-eval.rs
+++ examples/06-eval.rs
@@ -10,32 +10,39 @@ use nom::{
 };
 
 fn main() {
+    fn ex_eval<'src>(
+        input: &'src str,
+    ) -> Result<f64, nom::Err<nom::error::Error<&'src str>>> {
+        expr(input).map(|(_, e)| eval(e))
+    }
+
     let input = "123";
-    println!("source: {:?}, parsed: {:?}", input, expr(input));
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 
-    let input = "Hello + world";
-    println!("source: {:?}, parsed: {:?}", input, expr(input));
+    let input = "(123 + 456 ) + pi";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 
-    let input = "(123 + 456 ) + world";
-    println!("source: {:?}, parsed: {:?}", input, expr(input));
-
-    let input = "car + cdr + cdr";
-    println!("source: {:?}, parsed: {:?}", input, expr(input));
+    let input = "10 + (100 + 1)";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 
     let input = "((1 + 2) + (3 + 4)) + 5 + 6";
-    println!("source: {:?}, parsed: {:?}", input, expr(input));
-}
-
-#[derive(Debug, PartialEq, Clone)]
-enum Token<'src> {
-    Ident(&'src str),
-    Number(f64),
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Expression<'src> {
-    Value(Token<'src>),
+    Ident(&'src str),
+    NumLiteral(f64),
     Add(Box<Expression<'src>>, Box<Expression<'src>>),
+}
+
+fn eval(expr: Expression) -> f64 {
+    match expr {
+        Expression::Ident("pi") => std::f64::consts::PI,
+        Expression::Ident(id) => panic!("Unknown name {:?}", id),
+        Expression::NumLiteral(n) => n,
+        Expression::Add(lhs, rhs) => eval(*lhs) + eval(*rhs),
+    }
 }
 
 fn term(i: &str) -> IResult<&str, Expression> {
@@ -44,7 +51,7 @@ fn term(i: &str) -> IResult<&str, Expression> {
 
 fn ident(input: &str) -> IResult<&str, Expression> {
     let (r, res) = delimited(multispace0, identifier, multispace0)(input)?;
-    Ok((r, Expression::Value(Token::Ident(res))))
+    Ok((r, Expression::Ident(res)))
 }
 
 fn identifier(input: &str) -> IResult<&str, &str> {
@@ -58,12 +65,12 @@ fn number(input: &str) -> IResult<&str, Expression> {
     let (r, v) = delimited(multispace0, recognize_float, multispace0)(input)?;
     Ok((
         r,
-        Expression::Value(Token::Number(v.parse().map_err(|_| {
+        Expression::NumLiteral(v.parse().map_err(|_| {
             nom::Err::Error(nom::error::Error {
                 input,
                 code: nom::error::ErrorKind::Digit,
             })
-        })?)),
+        })?),
     ))
 }
 
