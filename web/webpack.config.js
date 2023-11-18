181759add55205fb0353eb9683a0f762a5e247f1
msakuta <masahiro.sakuta@gmail.com>
2023-11-18 19:37:08 +0900

    Add wasm build

---
+++ web/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        // Have this example work in Edge which doesn't ship `TextEncoder` or
+        // `TextDecoder` at this time.
+        new webpack.ProvidePlugin({
+          TextDecoder: ['text-encoding', 'TextDecoder'],
+          TextEncoder: ['text-encoding', 'TextEncoder']
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: '../assets', to: 'assets' }
+            ]
+        })
+    ],
+    mode: 'development',
+    experiments: {
+        asyncWebAssembly: true
+   }
+};
